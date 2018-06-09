527e8d4ddf8ad3a3f497ba956b21639f66e5ebef
gabkyo <34278677+gabkyo@users.noreply.github.com>
2018-06-08 22:24:04 -0300

    Add files via upload

--- classes.clj
+++ classes.clj
@@ -23,5 +23,25 @@
 	set (fn [i o c s p b1 b2 a]
           (do (reset! id i)
 			  (reset! owner o)
-			  (reset! categoria c)))
+			  (reset! categoria c)
+			  (reset! solo s)
+			  (reset! precom2 p)
+			  (reset! base1 b1)
+			  (reset! base2 b2)
+			  (reset! altura a)
+			  ))
+
+	(Area []
+		(if (= :categoria "triang")
+			(/ (* :base1 :altura) 2)
+			(if (= :categoria "retang")
+				(* :base1 :altura) 
+				(if (= :categoria "trapez")
+					(/ (* (+ base1 base2) altura) 2)
+					0))))
+
+	(Preco [] 
+		
+
+	)
 )
