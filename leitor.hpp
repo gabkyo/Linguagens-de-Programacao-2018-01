605e47e871c8cf772acd60858a84c0933f3737a8
gabkyo <34278677+gabkyo@users.noreply.github.com>
2018-06-14 18:43:05 -0300

    Add files via upload

--- leitor.hpp
+++ leitor.hpp
@@ -1,7 +1,36 @@
 #include "list.hpp"
 #include <fstream>
+#include <algorithm>
 
-void
- ler_catalogo(LList *lista){
+string trim(string a){
+  a.erase(remove(a.begin(),a.end(),'\n'),a.end());
+  return a;
+}
 
+void ler_catalogo(LList *lista){
+   ifstream catalogo ("catalogo.txt");
+   string linha, owner,categoria;
+   int id;
+   if (catalogo.is_open()) {
+     while (catalogo>>categoria) {
+       catalogo>>id;
+       catalogo>>owner;
+       if (categoria=="apto") {
+
+       }else if (categoria=="casa") {
+
+       }else{
+         string solo;
+         double pm2,b1,b2,altura;
+         catalogo>>solo;
+         catalogo>>pm2;
+         if (categoria=="trapez") {
+           catalogo>>b1;
+           catalogo>>b2;
+           catalogo>>altura;
+         }
+       }
+     }
+     catalogo.close();
+   }
 }
