012ef5ec7ba7e1ef525283833298bd066282294c
gabkyo <34278677+gabkyo@users.noreply.github.com>
2018-06-28 22:00:19 -0300

    done

--- lp4/src/lp4/list.clj
+++ lp4/src/lp4/list.clj
@@ -10,3 +10,13 @@
 
 (defn criarCasas [lista plimite alimite] ;;retorna lista com casas com preco < plim e area >alimite
 	(filter (fn [x] (and (and (= (Categoria x) "casa") (< (Preco x) plimite)) (> (Area x) alimite))) lista))
+
+(defn sortCasas [a b]
+	(compare [(Info b) (Id b)] [(Info a) (Id a)])) 
+	;;area decrescente id crescente
+
+(defn criarArgilosos [lista]
+	(filter (fn [x] (= (Info x) "G")) lista))
+
+(defn sortArgiloso [a b]
+	(compare [(Area b) (Id b)] [(Area a) (Id a)])) 
--- lp4/src/lp4/main.clj
+++ lp4/src/lp4/main.clj
@@ -7,6 +7,10 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (def lista (catalogo))
-  ;(def lista (sort sortImoveis lista))
-  (println lista))
+  (def lista (seq nil))
+  (def lista (catalogo lista))
+  (def lista (atual lista))
+  (def lista (sort sortImoveis lista))
+  ;(println lista)
+  ;(println (count lista))
+  (espec lista))
